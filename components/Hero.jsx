3958f4a6431df87bbcd3db340c92e098b89326d7
Elias Bello <eabelloc.dev@gmail.com>
2022-08-26 20:36:10 +0200

    Image_test-3

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="text-gray-400 bg-zinc-900 body-font">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
-          <img className="object-cover object-center rounded" alt="Elias Bello" src="https://i.imgur.com/ZANKuYk.jpg" />
+          <img className="object-cover object-center rounded" alt="Elias Bello" src="../public/images/coding.png" />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">Hello, I am Elias Bello 👋🏼!
