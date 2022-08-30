d9ca1648281fb8fbf507d6dc42b99f89a13cba35
Elias Bello <eabelloc.dev@gmail.com>
2022-08-30 13:02:09 +0200

    Some-small-changes

--- components/Head.jsx
+++ components/Head.jsx
@@ -11,14 +11,14 @@ const Head = () => {
     <NextHead>
       <title>Elias Bello - Site</title>
       <meta
-        content="Soy Senior Full Stack Developer y apasionado de las tecnologías basadas en Javascript."
+        content="I am a Junior Full Stack Developer and passionate about technologies based on JavaScript."
         name="description"
       />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Elias Bello - Website" />
       <meta
         property="og:description"
-        content="Soy Senior Full Stack Developer y apasionado de las tecnologías basadas en Javascript."
+        content="I am a Junior Full Stack Developer and passionate about technologies based on JavaScript."
       />
       <meta property="og:title" content="Elias Bello - Site" />
       <meta property="og:image" content={image} />
